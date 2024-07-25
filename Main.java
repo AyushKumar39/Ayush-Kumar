97fccfad260044d911ed4e8d55c3c8e2e6868b34
AyushKumar39 <156656451+AyushKumar39@users.noreply.github.com>
2024-07-25 18:28:21 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,20 @@
-import java.lang.*;
-import java.util.*;
+package com.carRentalSystem;
 
-class Array{
-    public static void main(String []args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of array : ");
-        int a=sc.nextInt();
-        int x[]=new int[a];
+public class Main {
+    public static void main(String[] args) {
+        CarRentalSystem rentalSystem=new CarRentalSystem();
+        Car car1 =new Car("c001","Toyota","Camry",60.0);
+        Car car2 =new Car("c002","Honda","Accord",70.0);
+        Car car3 =new Car("c003","Mahindra","Thar",160.0);
+        Car car4 =new Car("c004","Tata","nano",10.0);
+        Car car5 =new Car("c005","suzuki","xuv100",40.0);
 
-        for(int i=0;i<a;i++) {
-            x[i]=sc.nextInt();
-           // System.out.println("enter the element of array :" +z);
-        }
-        for(int i=0;i<x.length;i++){
-           System.out.println("enter the element of the array :" +x[i]);
-        }
+        rentalSystem.addCar(car1);
+        rentalSystem.addCar(car2);
+        rentalSystem.addCar(car3);
+        rentalSystem.addCar(car4);
+        rentalSystem.addCar(car5);
+        rentalSystem.menu();
+
     }
-}
+    }
